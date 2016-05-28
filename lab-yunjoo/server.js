fa2904c6ca3ed1467573f7957a99b3ebae5f0640
YUN JOO KIM <moniqueyj282@hotmail.com>
2016-05-28 14:47:47 -0700

    ready for test

---
+++ lab-yunjoo/server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const debug = require('debug')('note:server');
+const express = require('express');
+const bodyParser = require('body-parser');
+const noteRouter = require('./route/note-router');
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(bodyParser.json());
+app.use('/api/note',noteRouter);
+app.all('*', function(req, res){
+  debug('* 404');
+  res.status(404).send('not found');
+});
+
+const server = app.listen(port, function(){
+  debug('listen');
+  console.log('app up on port', port);
+});
+
+server.isRunning = true;
+module.exports = server;
